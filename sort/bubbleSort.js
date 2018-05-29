18a0dbab8e6d58c03f9147d6de8d74171768d641
Zeryl1 <36546322+Zeryl1@users.noreply.github.com>
2018-05-29 22:45:37 +0300

    Create bubbleSort.js

---
+++ sort/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr)
+{
+	var len = arr.length
+	for (var i = 0; i < len - 1; i++)
+		for (var j = 0; j < len - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+			{
+				var tmp = arr[j]
+				arr[j] = arr[j + 1]
+				arr[j + 1] = tmp
+			}
+	return arr
+}
